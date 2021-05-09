2e807f4e19d7c2d94bbffa1165cf737e518f1b78
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-05-09 13:28:40 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UploadComponent } from './upload/upload.component';
+import { InformationComponent } from './information/information.component';
 
 
 const routes: Routes = [
   { path: 'upload', component: UploadComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'information', component: InformationComponent },
   { path: '', component: HomeComponent },
 ];
 
